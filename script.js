3935d37bac8c9501eee58ff00daaf88473c2a59d
Ayrton54 <Ayrtonlopes54@gmail.com>
2024-11-21 21:19:05 -0300

    criação do cadastro e atualizações

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-document.querySelector('.login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    
-    const username = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value.trim();
-    
-    if(username === 'username' && password === 'username'){
-        window.location.href = 'home.html';
-    }
-    else {
-        alert('Usuário ou senha não reconhecidos.')
-    }
-});
+
+const loginForm = document.querySelector('.login-form');
+if (loginForm) {
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username').value.trim();
+        const password = document.getElementById('password').value.trim();
+
+        const userData = localStorage.getItem(username);
+
+        if (userData) {
+            const storedData = JSON.parse(userData);
+            if (storedData.password === password) {
+                alert('Login realizado com sucesso!');
+                window.location.href = '../home.html'; 
+            } else {
+                alert('Senha incorreta!');
+            }
+        } else {
+            alert('Usuário não encontrado!');
+        }
+    });
+}
+
+
+
+const cadastroForm = document.querySelector('.cadastroForm');
+if (cadastroForm) {
+    cadastroForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username-cadastro').value.trim();
+        const password = document.getElementById('password-cadastro').value.trim();
+
+        if (!username || !password) {
+            alert('Por favor, preencha todos os campos.');
+            return;
+        }
+
+        if (localStorage.getItem(username)) {
+            alert('Usuário já cadastrado!');
+        } else {
+            localStorage.setItem(username, JSON.stringify({ password }));
+            alert('Cadastro realizado com sucesso!');
+            window.location.href = '../login/index.html';
+        }
+    });
+}
